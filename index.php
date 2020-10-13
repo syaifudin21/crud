10da2adbc5e797e54aecee8f4143b7a385d71321
syaifudin.lenvoo <syaifudin@lenovo.com>
2020-10-13 15:39:19 +0700

    bootstrap

--- index.php
+++ index.php
@@ -9,12 +9,45 @@ $users = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 <html>
 <head>    
     <title>Homepage</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 
 <body>
+
+<div class="container">
 <a href="add.php">Add New User</a><br/><br/>
 
-    <table border=1px>
+<!-- <div class="row">
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+    <div class="col-1 border">1</div>
+</div>
+<br>
+<div class="row">
+    <div class="col-3 border">3</div>
+    <div class="col-3 border">3</div>
+    <div class="col-3 border">3</div>
+    <div class="col-3 border">3</div>
+</div>
+
+<br>
+
+<div class="row">
+    <div class="col-6 border">6</div>
+    <div class="col-6 border">6</div>
+</div>
+<br> -->
+
+    <table class="table table-borderless table-sm table-striped">
         <tr>
             <td>ID</td>
             <td>Nama</td>
@@ -36,5 +69,11 @@ $users = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
         ?>
         
     </table>
+
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
